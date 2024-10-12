51759313de840dacd0399f65750839faf28c23af
AntoineKin <antoine.kinable@gmail.com>
2024-10-12 15:40:45 +0200

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,9 +1,18 @@
 from pacman_module.game import Agent, Directions
-from pacman_module.util import PriorityQueue
-from pacman_module.util import manhattanDistance
+from pacman_module.util import PriorityQueue, manhattanDistance
+from pacman_module.pacman import GameState
 
 
 def key(state):
+    """Returns a key that uniquely identifies a Pacman game state.
+
+    Arguments:
+        state: a game state. See API or class `pacman.GameState`.
+
+    Returns:
+        A hashable key tuple.
+    """
+
     return (
         state.getPacmanPosition(),
         state.getFood(),
@@ -11,40 +20,24 @@ def key(state):
     )
 
 
-def forwardCost(state, current_capsules, initial_capsules):
-    pacman_pos = state.getPacmanPosition()
-    food_positions = state.getFood().asList()
-
-    if not food_positions:
-        return 0
-
-    dist = max(manhattanDistance(pacman_pos, food_pos) for food_pos in food_positions)
-
-    capsule = 100000 if len(current_capsules) < len(initial_capsules) else 0
-
-    return dist + capsule
-
-
-def backwardCost(path, current_capsules, initial_capsules):
-    cost = len(path)
-    
-    missing_capsules = len(initial_capsules) - len(current_capsules)
-    cost += missing_capsules * 100000 
-
-    return cost
-
-
-def estimatedCost(state, path, initial_capsules):
-    current_capsules = state.getCapsules()
-    return backwardCost(path, current_capsules, initial_capsules) + forwardCost(state, current_capsules ,initial_capsules)
-
-
 class PacmanAgent(Agent):
+    """Pacman agent based on A*."""
+
     def __init__(self):
         super().__init__()
+
         self.moves = None
 
     def get_action(self, state):
+        """Given a Pacman game state, returns a legal move.
+
+        Arguments:
+            state: a game state. See API or class `pacman.GameState`.
+
+        Return:
+            A legal move as defined in `game.Directions`.
+        """
+
         if self.moves is None:
             self.moves = self.astar(state)
 
@@ -53,30 +46,82 @@ class PacmanAgent(Agent):
         else:
             return Directions.STOP
 
-    def astar(self, state):
+    def compute_mst(self, positions, init_pos):
+        """Given a list of positions and an initial positions,
+        returns the minimum spanning tree toal cost,
+        where the cost of moving from a position
+        to another is the manhattan distance.
+
+        Arguments:
+            positions: a list of coordinates
+            init_pos: the initial coordinate
+
+        Returns:
+            The total cost of the minimum spanning tree
+        """
+        coordinates = set(positions)
+        visited = set()
+        total_cost = 0
         fringe = PriorityQueue()
-        initial_capsules = set(state.getCapsules())
-        fringe.push((state, [], initial_capsules), 0)
-        closed = set()
+        fringe.push(init_pos, priority=0)
 
         while not fringe.isEmpty():
-            _, (current_state, path, remaining_capsules) = fringe.pop()
+            cost, u = fringe.pop()
 
-            if current_state.isWin():
+            if u in visited:
+                continue
+
+            visited.add(u)
+            total_cost += cost
+
+            for v in coordinates.difference(visited):
+                dist = manhattanDistance(u, v)
+                fringe.push(v, priority=dist)
+
+        return total_cost
+
+    def astar(self, state: GameState):
+        """Given a Pacman game state, returns a list of legal moves to solve
+        the search layout.
+
+        Arguments:
+            state: a game state. See API or class `pacman.GameState`.
+
+        Returns:
+            A list of legal moves.
+        """
+
+        path = []
+        fringe = PriorityQueue()
+        fringe.push((state, path), priority=0)
+        closed = set()
+        initial_capsules = state.getCapsules()
+
+        while True:
+            if fringe.isEmpty():
+                return []
+
+            _, (current, path) = fringe.pop()
+
+            if current.isWin():
                 return path
 
-            current_key = key(current_state)
+            current_key = key(current)
 
             if current_key in closed:
                 continue
+            else:
+                closed.add(current_key)
 
-            closed.add(current_key)
+            food_positions = set(current.getFood().asList())
+            for successor, action in current.generatePacmanSuccessors():
+                pacman_pos = successor.getPacmanPosition()
 
-            for successor, action in current_state.generatePacmanSuccessors():
-                new_path = path + [action]
-                new_capsules = set(successor.getCapsules())
+                forward_cost = self.compute_mst(food_positions, pacman_pos)
 
-                cost = estimatedCost(successor, new_path, initial_capsules)
-                fringe.push((successor, new_path, new_capsules), cost)
+                backward_cost = len(path) + 1 +\
+                    5*((len(initial_capsules) - len(successor.getCapsules())))
 
-        return []
+                total_cost = forward_cost + backward_cost
+
+                fringe.push((successor, path + [action]), priority=total_cost)
